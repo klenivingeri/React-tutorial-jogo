c0f19b12ada4ee8a7ab81a5ebca10abd73fa3a03
Erick <libertti@live.com>
2021-02-05 11:56:06 -0300

    Componente interativo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={()=>{alert('click');}}>
         {this.props.value}
       </button>
     );
