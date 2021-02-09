20a907a1a69a522327da61b57b3a636718b5bcb3
Erick <libertti@live.com>
2021-02-08 23:58:38 -0300

    Ganhador

--- src/index.js
+++ src/index.js
@@ -32,14 +32,15 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice()
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : '0';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
     })
   } 
-
-
 
   renderSquare(i) {
     return (
@@ -51,11 +52,17 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player:' + (this.state.xIsNext ? '1' : '2');
+    const winner = calculateWinner(this.state.squares)
+    let status;
+    if(winner){
+      status = 'Winner:' + winner;
+    }else{
 
+    }
+    const play = 'Next player:' + (this.state.xIsNext ? '1' : '2')
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{play}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -71,7 +78,9 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <div className="status">{status}</div>
       </div>
+      
     );
   }
 }
@@ -98,3 +107,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
